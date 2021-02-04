93e4ae9beb2e45ee75fb354f405f6ab8f00f6270
Bridger Herman <herma582@umn.edu>
2021-02-04 11:36:44 -0600

    Add bounds methods

---
+++ Runtime/Gizmos/BoundsOutline.cs
@@ -0,0 +1,31 @@
+﻿using IVLab.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsOutline : MonoBehaviour
+{
+    public static BoundsOutline Init(GameObject go)
+    {
+        return go.AddComponent<BoundsOutline>();
+    }
+    public Bounds ToOutline { get; set; }
+    // private void OnGUI()
+    // {
+    //     _bounds = BoundedObject?.ToOutline ?? default;
+    // }
+
+    // Start is called before the first frame update
+    void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        if(ToOutline != null)
+        Gizmos.DrawWireCube(ToOutline.center, ToOutline.size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
