e1dc95770cf044054cfe7ab49a282a8bbf8f0578
Bridger Herman <herma582@umn.edu>
2021-02-07 14:44:54 -0600

    Add generalization to expand bounding box

--- Runtime/SpaceTransforms/NormalizeWithinBounds.cs
+++ Runtime/SpaceTransforms/NormalizeWithinBounds.cs
@@ -33,5 +33,41 @@ namespace IVLab.Utilities
 
             contained = new Bounds(container.center, toBeContained.size * scaleFactor);
         }
+
+        /// <summary>
+        ///     Normalize a bounds object to be contained within another, but
+        ///     also expand it within that container if necessary. Useful for
+        ///     importing new data objects with a common reference frame and the
+        ///     new one is bigger than the previous one.
+        /// </summary>
+        /// <returns>
+        ///     Boolean whether or not the bounds were expanded.
+        /// </returns>
+        public static bool NormalizeAndExpand(
+            Bounds container, // The result will be contained within these bounds
+            Bounds thisDataSpaceBounds, // The original data bounds of this data object
+            ref Bounds boundsAll, // In room (container) space, the current bounds of all data objects (not modified if not expanded)
+            ref Matrix4x4 transform, // The resulting transformation (not modified if not expanded)
+            ref Bounds dataSpaceBoundsAll // In data space, the current bounds of all data objects together
+        )
+        {
+            // Get the scale and bounds after trying to fit the dataset within the
+            // DataContainer
+            Matrix4x4 tmpTransform;
+            Bounds containedBounds;
+            NormalizeWithinBounds.Normalize(container, thisDataSpaceBounds, out tmpTransform, out containedBounds);
+
+            // If the new bounds would exceed the size of the current data
+            // bounds, reassign the data transform
+            bool expand = thisDataSpaceBounds.size.MaxComponent() > dataSpaceBoundsAll.size.MaxComponent();
+            if (expand)
+            {
+                boundsAll.Encapsulate(containedBounds);
+                transform = tmpTransform;
+            }
+            dataSpaceBoundsAll.Encapsulate(thisDataSpaceBounds);
+
+            return expand;
+        }
     }
 }
