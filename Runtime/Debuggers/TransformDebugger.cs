9ca9fe5ff6e752b694314511422533862ceb8452
Bridger Herman <herma582@umn.edu>
2021-02-18 20:16:29 -0600

    Add transform debugger

---
+++ Runtime/Debuggers/TransformDebugger.cs
@@ -0,0 +1,27 @@
+/* Debug transforms by attaching this to an object. Will display local/world
+position, rotation, and scale in the units that Unity uses internally instead of
+nicifying it for the editor.*/
+
+using UnityEngine;
+
+public class TransformDebugger : MonoBehaviour
+{
+    public Vector3 localPosition;
+    public Vector3 worldPosition;
+
+    public Quaternion localRotation;
+    public Quaternion worldRotation;
+
+    public Vector3 localScale;
+
+    void LateUpdate()
+    {
+        localPosition = transform.localPosition;
+        worldPosition = transform.position;
+
+        localRotation = transform.localRotation;
+        worldRotation = transform.rotation;
+
+        localScale = transform.localScale;
+    }
+}
